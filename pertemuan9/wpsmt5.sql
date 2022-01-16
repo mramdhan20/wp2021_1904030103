1b727c71871023fe66058a598c9c4843fff412ea
mramdhan20 <mramdhan2012@gmail.com>
2022-01-16 20:19:46 +0700

    pertemuan 9, index & detail

---
+++ pertemuan9/wpsmt5.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Des 2021 pada 05.03
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wpsmt5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_mhs`
+--
+
+CREATE TABLE `calon_mhs` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(9) NOT NULL,
+  `agama` varchar(9) NOT NULL,
+  `sekolah_asal` varchar(20) NOT NULL,
+  `foto_maba` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `calon_mhs`
+--
+
+INSERT INTO `calon_mhs` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`, `foto_maba`) VALUES
+(1, 'muhammad ramdhan', 'pasar kemis', 'laki laki', 'Islam', 'SMK AL-Ijtihad', 0),
+(2, 'randy rizky prasetyo', 'cimone', 'laki laki', 'Islam', 'SMK Bhakti Anindya', 0),
+(3, 'muhammad farhan ramadhan', 'priuk', 'laki laki', 'Islam', 'SMA 4', 0),
+(4, 'septian agus nur fajri', 'cikupa', 'laki laki', 'islam', 'SMK 1', 0),
+(5, 'okky tri mulyanto', 'cikokol', 'laki laki', 'Islam', 'SMA 2', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
