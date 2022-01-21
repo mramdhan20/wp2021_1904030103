ab69f0e1830a36a34c2ea070c203cfef1a35cd1f
mramdhan20 <mramdhan2012@gmail.com>
2022-01-21 19:59:25 +0700

    pertemuan 11 ,Edit & pencarian data

---
+++ pertemuan11/hapus.php
@@ -0,0 +1,22 @@
+<?php
+require "function.php";
+
+// agar tidak bisa masuk secara pengetikan manual pada url
+if (!isset($_GET['id'])) {
+    header("location: index.php");
+    exit;
+}
+
+$id = $_GET['id'];
+if (hapus($id) > 0) {
+    echo
+    "<script>
+        alert('data berhasil di hapus');
+        document.location.href = 'index.php';
+        </scrupt>";
+} else {
+    echo
+    "<script>
+        alert('data gagal dihapus');
+        </scrupt>";
+}
